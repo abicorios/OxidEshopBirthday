a5a3bbdde5ca7fbd14c3a4dea21550a97c8515c8
abicorios <yaroslav.moroz91@gmail.com>
2023-08-14 19:49:40 +0300

    Add metadata

---
+++ metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'userbirthday',
+    'title'       => [
+        'de' => 'OXID Academy - Geburtstagsgrüße',
+        'en' => 'OXID Academy - birthday greetings',
+    ],
+    'description' => [
+        'de' => 'Sendet einen Geburtstagsgrüße an den Kunden, wenn er Geburtstag hat.',
+        'en' => 'Generate birthday greetings when birthday.',
+    ],
+    'thumbnail'   => '',
+    'version'     => '1.0',
+    'author'      => 'Oxid Academy',
+    'url'         => 'https://www.oxid-esales.com/',
+    'email'       => 'academy@oxid-esales.com',
+    'extend'      => [],
+    'blocks'      => [],
+    'settings'    => [],
+];

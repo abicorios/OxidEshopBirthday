0bc2e6e925ad69d53b7a62da605b467865f459c6
abicorios <yaroslav.moroz91@gmail.com>
2023-08-14 21:11:07 +0300

    Add the lang file

---
+++ views/admin/en/birthday_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$sLangName = 'English';
+$iLangNr = 1;
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    "charset" => "UTF-8",
+    "SHOP_MODULE_GROUP_OXAC_BIRTHDAY_TEXT" => "text settings",
+    "SHOP_MODULE_sBirthdayTextStart" => "birthday text",
+];

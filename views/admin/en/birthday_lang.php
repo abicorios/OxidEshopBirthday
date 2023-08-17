be4d3099105f1851b6435128e197114f89a9f42a
abicorios <yaroslav.moroz91@gmail.com>
2023-08-17 18:37:32 +0300

    Update quotas

--- views/admin/en/birthday_lang.php
+++ views/admin/en/birthday_lang.php
@@ -6,7 +6,7 @@ $iLangNr = 1;
 // RESOURCE IDENTIFIER = STRING
 // -------------------------------
 $aLang = [
-    "charset" => "UTF-8",
-    "SHOP_MODULE_GROUP_OXAC_BIRTHDAY_TEXT" => "text settings",
-    "SHOP_MODULE_sBirthdayTextStart" => "birthday text",
+    'charset' => 'UTF-8',
+    'SHOP_MODULE_GROUP_OXAC_BIRTHDAY_TEXT' => 'text settings',
+    'SHOP_MODULE_sBirthdayTextStart' => 'birthday text',
 ];
